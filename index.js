a914ef2368eb3a6cac0d13aa8828257913b106eb
Chikitonik <chikitonik@gmail.com>
2023-01-11 19:16:16 +0200

    init

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const fs = require("fs");
+const express = require("express");
+const cors = require("cors");
 
 const token = JSON.parse(fs.readFileSync("./botData.json", "utf8"))[0].token;
 const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(token, { polling: true });
+
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 const webAppUrl = "https://jocular-tartufo-23c9c3.netlify.app";
 
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
+
+  console.log("msg >>", msg);
 
   if (text === "/start") {
     await bot.sendMessage(chatId, "Press the button below and make an order", {
@@ -22,7 +30,7 @@ bot.on("message", async (msg) => {
   if (msg?.web_app_data?.data) {
     try {
       const data = JSON.parse(msg?.web_app_data?.data);
-
+      console.log(data);
       await bot.sendMessage(chatId, "Thanks for Your reply");
       await bot.sendMessage(chatId, "Your country is " + data?.country);
       await bot.sendMessage(chatId, "Your street is " + data?.street);
@@ -38,3 +46,31 @@ bot.on("message", async (msg) => {
     }
   }
 });
+
+app.post("web-data", async (req, res) => {
+  const { queryId, products, totalPrice } = req.body;
+  try {
+    await bot.answerWebAppQuery(queryId, {
+      type: "article",
+      id: queryId,
+      title: "successful purchase",
+      input_message_content: {
+        message_text: "You purchased goods worth " + totalPrice,
+      },
+    });
+    return res.status(200).json({});
+  } catch (e) {
+    await bot.answerWebAppQuery(queryId, {
+      type: "article",
+      id: queryId,
+      title: "purchase failed",
+      input_message_content: {
+        message_text: "purchase failed",
+      },
+    });
+    return res.status(500).json({});
+  }
+});
+
+const PORT = 8000;
+app.listen(PORT, () => console.log("server started on " + PORT));
